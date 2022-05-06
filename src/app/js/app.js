225fca8f4a8b412ba1c345b031aedd0f8f5265da
Tesfa Demissie <tesfadaniel1@gmail.com>
2022-05-06 10:58:24 -0500

    Initial commit

---
+++ src/app/js/app.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import Pomodoro from './../components/Pomodoro/Pomodoro.jsx';
+
+ReactDom.render( < Pomodoro / > , document.getElementById('app'));
